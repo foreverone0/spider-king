dc9b8bca87e7c14c4967bd916c12170a94239196
gdi <gdi3478@hdl.com>
2024-02-24 16:07:07 +0800

    cookies_to_dict

--- spider_king/modules/helper.py
+++ spider_king/modules/helper.py
@@ -16,3 +16,17 @@ def get_size_str(size: int):
         return f"{size / 1024 / 1024:.2f}MB"
     else:
         return f"{size / 1024 / 1024 / 1024:.2f}GB"
+
+
+def cookies_to_dict(cookies):
+    """
+    将cookies字符串转换为字典
+
+    :param cookies:
+    :return:
+    """
+    cookies = cookies.split(";")
+    cookies = [cookie.strip() for cookie in cookies]
+    cookies = [cookie.split("=", 1) for cookie in cookies]
+    cookies = {cookie[0]: cookie[1] for cookie in cookies if len(cookie) == 2}
+    return cookies
